f53f621d50a1a67fc3e2fb8e38d0404d6167cf56
장민호 <minho9301@naver.com>
2020-12-07 16:56:53 +0900

    완강: Vue.js 시작하기 - Age of Vue.js

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-header 
+      v-bind:msg="headerMsg"
+      v-on:renew="renewStr">
+    </app-header>
+    {{ str }}
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  // 변수를 function 형태로 리턴해야함.
+  data: function() {
+    return {
+      str: 'hi',
+      headerMsg: 'APP HEADER'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'Hello World !!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>p
